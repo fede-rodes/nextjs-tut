77c6b53ec5147c08abacee865bf9d274fccb6e31
fede-rodes <federodes@gmail.com>
2022-03-10 18:25:52 +0100

    webpack config working

---
+++ stories/Home.stories.tsx
@@ -0,0 +1,26 @@
+import Home from '../pages/index'
+
+export default {
+  title: 'Pages/Home',
+  component: Home,
+}
+
+const mockPosts = [
+  {
+    id: 'post_one',
+    date: '2020/03/01',
+    title: 'Post One',
+  },
+  {
+    id: 'post_two',
+    date: '2020/04/01',
+    title: 'Post Two',
+  },
+  {
+    id: 'post_three',
+    date: '2020/05/01',
+    title: 'Post Three',
+  },
+]
+
+export const HomePage = () => <Home posts={mockPosts} />
